5c3f5c6ca837c016d4d82619fe0b57a12afbffce
function2 <function2@qq.com>
2019-11-12 00:48:44 +0800

    check sum

--- Homework/checksum/checksum.cpp
+++ Homework/checksum/checksum.cpp
@@ -1,13 +1,22 @@
-#include <stdint.h>
-#include <stdlib.h>
+#include <limits>
+#include <cassert>
+#include <cstdlib>
+#include <cstdint>
+#include <iostream>
 
-/**
- * @brief 进行 IP 头的校验和的验证
- * @param packet 完整的 IP 头和载荷
- * @param len 即 packet 的长度，单位是字节，保证包含完整的 IP 头
- * @return 校验和无误则返回 true ，有误则返回 false
- */
+uint16_t big(uint8_t* val) { return (uint16_t(val[0]) << 8) + val[1]; }
+
 bool validateIPChecksum(uint8_t *packet, size_t len) {
-  // TODO:
-  return true;
+    using std::cerr;
+    using std::endl;
+    uint32_t checksum = 0;
+    uint8_t IHL = packet[0] & 0xf;
+    for (size_t i = 0; i < IHL * 4; i += 2) {
+        if (i == 10) continue;
+        checksum += big(packet + i);
+    }
+    while (checksum > 0xffff)
+        checksum = (uint16_t(checksum) & 0xffff) + (checksum >> 16);
+    uint16_t actual = ~big(packet + 10);
+    return checksum == actual;
 }
