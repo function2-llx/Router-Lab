0697c11e0a0594f88fcd23042899376a698a2efd
function2 <function2@qq.com>
2019-11-12 08:15:43 +0800

    forward

--- Homework/forwarding/forwarding.cpp
+++ Homework/forwarding/forwarding.cpp
@@ -1,16 +1,32 @@
 #include <stdint.h>
 #include <stdlib.h>
 
-/**
- * @brief 进行转发时所需的 IP 头的更新：
- *        你需要先检查 IP 头校验和的正确性，如果不正确，直接返回 false ；
- *        如果正确，请更新 TTL 和 IP 头校验和，并返回 true 。
- *        你可以从 checksum 题中复制代码到这里使用。
- * @param packet 收到的 IP 包，既是输入也是输出，原地更改
- * @param len 即 packet 的长度，单位为字节
- * @return 校验和无误则返回 true ，有误则返回 false
- */
+uint16_t big(uint8_t* val) { return (uint16_t(val[0]) << 8) + val[1]; }
+
 bool forward(uint8_t *packet, size_t len) {
-  // TODO:
-  return false;
+    uint32_t checksum = 0;
+    uint8_t IHL = packet[0] & 0xf;
+    for (size_t i = 0; i < IHL * 4; i += 2) {
+        if (i == 10) continue;
+        checksum += big(packet + i);
+    }
+    while (checksum > 0xffff)
+        checksum = (uint16_t(checksum) & 0xffff) + (checksum >> 16);
+    uint16_t actual = ~big(packet + 10);
+    if (checksum == actual) {
+        packet[8]--;
+        checksum = 0;
+        for (size_t i = 0; i < IHL * 4; i += 2) {
+            if (i == 10) continue;
+            checksum += big(packet + i);
+        }
+        while (checksum > 0xffff)
+            checksum = (uint16_t(checksum) & 0xffff) + (checksum >> 16);
+        checksum = ~checksum;
+        packet[10] = checksum >> 8;
+        packet[11] = checksum & 0xff;
+        return 1;
+    } else {
+        return 0;
+    }
 }
